ae4f95eeaf7cebc0614c8de128fe7c7ce227eb72
hv11285 <federico.carbonetti@devk.de>
2022-02-25 20:29:38 +0100

    +add RBAC to kubectl script ~edit README.md

--- kubectl/script.sh
+++ kubectl/script.sh
@@ -20,6 +20,14 @@ kubectl get csr lancelot -o jsonpath='{.status.certificate}' | base64 --decode >
 
 kubectl create namespace round-table
 
+kubectl create clusterrole pod-reader --verb=get,list,watch --resource=pods,pods/log
+kubectl create clusterrole node-reader --verb=get,list,watch --resource=nodes
+kubectl create role deployment-admin -n round-table --verb='*' --resource=deployments
+
+kubectl create rolebinding -n round-table pod-reader --clusterrole=pod-reader --group=knights
+kubectl create rolebinding -n round-table deployment-admin --role=deployment-admin --group=knights
+kubectl create clusterrolebinding node-reader --clusterrole=node-reader --user=lancelot
+
 kubectl config set-credentials lancelot --client-certificate=lancelot.crt --client-key=lancelot.key --embed-certs
 kubectl config set-context lancelot@kubernetes --user=lancelot --cluster=kubernetes --namespace=round-table
 kubectl config use-context lancelot@kubernetes

